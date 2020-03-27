e7d86ed2d5921db988c1ef9f68e807da0b9190a9
kimsanghoon1 <sanghoon01@uengine.org>
2020-03-27 14:30:05 +0900

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,2 @@
+wget -O /home/project/project.zip https://firebasestorage.googleapis.com/v0/b/eventstorming-tool-db.appspot.com/o/test%2Flocalaa.zip?alt=media&token=6c9b63d8-7ed0-41ad-9757-564112b3546f && \
+unzip /home/project/project.zip
